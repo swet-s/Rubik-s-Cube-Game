018d1af9c4de82bb29f0604b229b4b68310d9197
s-swet <shreet.swet@gmail.com>
2022-07-23 02:38:14 +0530

    Code refactoring & implemented get_fps()

--- Puzzles_3D/Game.py
+++ Puzzles_3D/Game.py
@@ -1,30 +1,21 @@
 import pygame
 from Puzzles_3D import Cube, GameInputs
 from Engine_3D.Vector import Vector3 as V3
-from Puzzles_3D.Screen import screen
+from Puzzles_3D.Screen import screen, get_fps
 
-rubik = Cube.Cube(V3(0, 0, 0), 200, 2)
+rubik = Cube.Cube(V3(0, 0, 0), 200, 3)
 Moves = GameInputs.GameInputs(rubik)
 
-
 font = pygame.font.Font("freesansbold.ttf", 20)
-lasttick = 0
 
 running = True
+
 # Game loop
 while running:
-    thistick = pygame.time.get_ticks()
-    if thistick-lasttick != 0:
-        fps = int(1000/(thistick - lasttick))
-    else:
-        fps = "Very High"
-    lasttick = thistick
-
     screen.fill((0, 0, 0))
-    screen.blit(font.render(f'{fps}', True, (155, 155, 00)), (10, 10))
+    screen.blit(font.render(f'{get_fps()}', True, (155, 155, 00)), (10, 10))
 
     rubik.draw_cube()
-
     pygame.display.update()
 
     for event in pygame.event.get():
@@ -34,4 +25,3 @@ while running:
     Moves.Rotate_from_mouse()
 
 pygame.quit()
-
--- Puzzles_3D/Screen.py
+++ Puzzles_3D/Screen.py
@@ -4,25 +4,37 @@ import pygame
 pygame.init()
 scr_width = 600
 scr_height = 600
+time = 0
 Centre_X = scr_width / 2
 Centre_Y = scr_height / 2
 # create the screen
 screen = pygame.display.set_mode((scr_width, scr_height))
 
 
-def Displace_z(point, cam=800):
+def get_fps():
+    global time
+    new_time = pygame.time.get_ticks()
+    if new_time - time != 0:
+        fps = int(1000 / (new_time - time))
+    else:
+        fps = "Very High"
+    time = new_time
+    return fps
+
+
+def displace_z(point, cam=800):
     dx, dy, dz = point
     if dz < cam:
-        pix_X = (dx * cam) / (cam - dz) + Centre_X
-        pix_Y = (-dy * cam) / (cam - dz) + Centre_Y
+        pix_x = (dx * cam) / (cam - dz) + Centre_X
+        pix_y = (-dy * cam) / (cam - dz) + Centre_Y
     else:
-        pix_X = 800
-        pix_Y = 600
-    return round(pix_X), round(pix_Y)
+        pix_x = 800
+        pix_y = 600
+    return round(pix_x), round(pix_y)
 
 
 def polygon3D(color, corners, width=0):
     PointList2D = []
     for v3 in corners:
-        PointList2D.append(Displace_z(v3))
+        PointList2D.append(displace_z(v3))
     pygame.draw.polygon(screen, color, PointList2D, width)
